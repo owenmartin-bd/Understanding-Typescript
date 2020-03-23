bb76e15aa8cc2b431f9e102e94f65437bf0970c2
owenmartin-bd <owen_0918@live.com>
2020-03-23 14:28:33 +0600

    day 2

--- Section 04/src/app.ts
+++ Section 04/src/app.ts
@@ -1,1 +1,46 @@
 // Code goes here!
+// const userName = 'Owen';
+// userName = 'Nartin';
+
+// let age = 30;
+
+// age = 29;
+
+// function add(a: number, b: number) {
+//     var result;
+//     result = a + b;
+//     return result;
+// }
+
+// const add = (a: number, b: number) =>  a+b;
+
+// console.log(add(2,5));
+
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+    firstName: 'Owen',
+    age: 35
+}
+
+const copiedPerson = {...person};
+
+const add = (...numbers: number[]) => {
+    return numbers.reduce((curReslut, curValue) => {
+        return curReslut + curValue;
+    }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies, hobby1, hobby2, remainingHobbies);
+
+const {firstName: userName, age} = person;
+
+console.log(userName, age);
